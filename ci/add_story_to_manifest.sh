ccafd7bb9c7ab557e072912d953a4c8e4dfa3c68
britz <jbritz@pivota.io>
2017-04-20 16:57:05 -0700

    One more try on those pesky env vars
    maybe [completes #144068571]

--- ci/add_story_to_manifest.sh
+++ ci/add_story_to_manifest.sh
@@ -3,5 +3,5 @@
 MANIFEST_TO_MODIFY=$1
 REVISION_SHA=$2
 
-awk -v REVISION_SHA=${REVISION_SHA} '/^- storySha:/{ $0 = "- storySha: " REVISION_SHA }{print}' ${MANIFEST_TO_MODIFY} > manifest.yml
+awk -v REVISION_SHA=${REVISION_SHA} '/^    storySha:/{ $0 = "    storySha: " REVISION_SHA }{print}' ${MANIFEST_TO_MODIFY} > manifest.yml
 
--- tracker.sh
+++ tracker.sh
@@ -3,6 +3,10 @@
 read -p "Tracker Story ID (e.g. #118149757): " TRACKER_ID
 
 echo "pivotalTrackerStoryID=$TRACKER_ID" > "./src/main/resources/tracker.properties"
+
+awk -v TRACKER_ID=${TRACKER_ID} '/^pivotalTrackerStoryID=/{ $0 = "pivotalTrackerStoryID=" TRACKER_ID }{print}' "./src/main/resources/tracker.properties" > "./src/main/resources/tracker.properties.new"
+mv "./src/main/resources/tracker.properties.new"  "./src/main/resources/tracker.properties"
+
 git add ./src/main/resources/tracker.properties
 
 git commit "$@"
