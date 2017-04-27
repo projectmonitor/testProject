c9431d8f8968bc9fcf97deb4e037a9a6a6df87d2
britz <jbritz@pivota.io>
2017-04-26 17:01:19 -0700

    Updating manifests to include story number being deployed
    [completes #144414719]

--- ci/add_story_to_manifest.sh
+++ ci/add_story_to_manifest.sh
@@ -2,6 +2,16 @@
 
 MANIFEST_TO_MODIFY=$1
 REVISION_SHA=$2
+STORY_ID=$3
 
-awk -v REVISION_SHA=${REVISION_SHA} '/^    storySha:/{ $0 = "    storySha: " REVISION_SHA }{print}' ${MANIFEST_TO_MODIFY} > manifest.yml
+#awk -v REVISION_SHA=${REVISION_SHA} '/^    storySha:/{ $0 = "    storySha: " REVISION_SHA }{print}' ${MANIFEST_TO_MODIFY} > manifest.yml
+
+line_number=$(cat -n manifest-acceptance.yml | grep storySha | awk '{print $1}')
+sed "${line_number}s/.*/    storySha: ${REVISION_SHA}/" manifest-acceptance.yml > manifesttemp.yml
+
+line_number=$(cat -n manifest.yml | grep pivotalTrackerStoryID | awk '{print $1}')
+sed "${line_number}s/.*/    pivotalTrackerStoryID: ${STORY_ID}/" manifesttemp.yml > manifest.yml
+
+rm manifesttemp.yml
+cat manifest.yml
 
