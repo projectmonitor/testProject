54a597024dfe682db999a46ffacad7ed045f96c2
britz <jbritz@pivota.io>
2017-04-27 10:06:49 -0700

    Monsta Mack
    [completes #144470047]

--- ci/add_story_to_manifest.sh
+++ ci/add_story_to_manifest.sh
@@ -2,6 +2,14 @@
 
 MANIFEST_TO_MODIFY=$1
 REVISION_SHA=$2
+STORY_ID=$3
 
-awk -v REVISION_SHA=${REVISION_SHA} '/^    storySha:/{ $0 = "    storySha: " REVISION_SHA }{print}' ${MANIFEST_TO_MODIFY} > manifest.yml
+line_number=$(cat -n ${MANIFEST_TO_MODIFY} | grep storySha | awk '{print $1}')
+sed "${line_number}s/.*/    storySha: ${REVISION_SHA}/" ${MANIFEST_TO_MODIFY} > manifesttemp.yml
 
+line_number=$(cat -n manifesttemp.yml | grep pivotalTrackerStoryID | awk '{print $1}')
+sed "${line_number}s/.*/    pivotalTrackerStoryID: ${STORY_ID}/" manifesttemp.yml > manifest.yml
+
+rm manifesttemp.yml
+
+cat manifest.yml
